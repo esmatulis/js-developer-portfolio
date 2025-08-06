dd22dfcfcc1b1855d0d01ac68464a95bb99aa116
Erica Matulis <erica.matulis2@gmail.com>
2025-08-05 22:08:08 -0300

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = '../data/profile.json';
+    const url = 'https://raw.githubusercontent.com/esmatulis/js-developer-portfolio/refs/heads/main/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
